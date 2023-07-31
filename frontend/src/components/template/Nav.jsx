fde07de10f1989b4fa822661fe096ad4d8c613d0
Eduardo Pires <econdepires@gmail.com>
2023-07-31 15:37:32 -0300

    Componente Navegação

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -2,6 +2,14 @@ import React from "react"
 import './Nav.css'
 
 export default props =>
-    <aside className="menuArea">
-        Navegação
+    <aside className="menu-area">
+        <nav className="menu">
+            {/* refatoração indicada */}
+            <a href="#/">
+                <i className="fa fa-home"></i> Início
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i> Usuários
+            </a>
+        </nav>
     </aside>
