992a4f35fee0676848af89272b7fe3012f60aa3a
Eduardo Pires <eduardo.c.pires@outlook.com>
2023-07-31 15:51:14 -0300

    Componente Conteúdo

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,18 +1,16 @@
 import React from "react"
 import Footer from '../components/template/Footer'
+import Home from "../components/home/Home"
 import Logo from '../components/template/Logo'
-import Main from '../components/template/Main'
 import Nav from '../components/template/Nav'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 
-// commit test
-
 export default props =>
     <div className="app">
         <Logo />
         <Nav />
-        <Main icon="home" subtitle="Segundo projeto do capítulo de React." title="Início" />
+        <Home></Home>
         <Footer />
     </div>
