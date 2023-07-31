d6478d78732070cd083d242cdf7bc7e11f1cd1a1
Eduardo Conde Pires <eduardo.c.pires@outlook.com>
2023-07-31 10:19:22 -0300

    Componente Cabeçalho

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React from "react"
 import Footer from '../components/template/Footer'
 import Logo from '../components/template/Logo'
 import Main from '../components/template/Main'
 import Nav from '../components/template/Nav'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 
 export default props =>
     <div className="app">
         <Logo />
         <Nav />
-        <Main />
+        <Main icon="home" subtitle="Segundo projeto do capítulo de React." title="Início" />
         <Footer />
     </div>
